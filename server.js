69cc45503764c78e11fb034262d883058911cd6b
laybson <laybson.cunha@ccc.ufcg.edu.br>
2019-10-11 18:35:20 -0300

    Up and running

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const itemsRouter = require('./routes/items');
 app.use('/items', itemsRouter);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
